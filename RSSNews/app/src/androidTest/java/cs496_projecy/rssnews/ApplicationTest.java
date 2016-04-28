090befae8b00152e9b1b16a3a5e619d2e78e185d
Devin Foulger <foulgerd@oregonstate.edu>
2016-04-28 13:51:00 -0700

    new project

---
+++ RSSNews/app/src/androidTest/java/cs496_projecy/rssnews/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cs496_projecy.rssnews;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ RSSNews/app/src/main/java/cs496_projecy/rssnews/MainFeed.java
@@ -0,0 +1,13 @@
+package cs496_projecy.rssnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_feed);
+    }
+}
