e73de5ccec114b962e8193594ceb136ff1f47351
David Winkler <winkleda@onid.orst.edu>
2016-04-29 16:42:25 -0700

    Added Class for ListView Adapter

--- RSSNews/app/src/main/java/cs496_projecy/rssnews/MainFeed.java
+++ RSSNews/app/src/main/java/cs496_projecy/rssnews/MainFeed.java
@@ -2,12 +2,35 @@ package cs496_projecy.rssnews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainFeed extends AppCompatActivity {
+    ArrayList<String> myStrArr;
+    ListView mList;
+    MyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_feed);
+
+        mList = (ListView)findViewById(R.id.mainList);
+
+        // test
+        myStrArr = new ArrayList<String>();
+        myStrArr.add("String 1");
+        myStrArr.add("String 2");
+        myStrArr.add("String 3");
+
+        // Create an adapter and attach it to the list
+        mAdapter = new MyAdapter(this, myStrArr);
+        mList.setAdapter(mAdapter);
+
+        mAdapter.addAll(myStrArr);
     }
+
+
 }
---
+++ RSSNews/app/src/main/java/cs496_projecy/rssnews/MyAdapter.java
@@ -0,0 +1,48 @@
+package cs496_projecy.rssnews;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by David on 4/29/2016.
+ */
+public class MyAdapter extends ArrayAdapter<String> {
+    int count = 0;
+
+    public MyAdapter(Context context, ArrayList<String> items) {
+        super(context, 0, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the item we are trying to represent
+        String item = getItem(position);
+
+        // Check if we are recycling an existing view
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        // Now get the data fields for the list_item view:
+        TextView title = (TextView)convertView.findViewById(R.id.title);
+        TextView summary = (TextView)convertView.findViewById(R.id.summary);
+        ImageView image = (ImageView)convertView.findViewById(R.id.image);
+
+        // Now add our data into the view
+        title.setText("This is title #" + count);
+        summary.setText(item);
+        image.setImageResource(R.mipmap.ic_launcher);
+
+        // Finish
+        return convertView;
+    }
+
+
+}
