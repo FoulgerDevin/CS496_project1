b1b2ba27135819427353b939512cfaa8a3afd7dc
David Winkler <winkleda@onid.orst.edu>
2016-05-05 17:46:14 -0700

    Added MainService.java

--- RSSNews/app/src/main/java/cs496_projecy/rssnews/MainFeed.java
+++ RSSNews/app/src/main/java/cs496_projecy/rssnews/MainFeed.java
@@ -1,7 +1,11 @@
 package cs496_projecy.rssnews;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.AsyncTask;
+import android.os.IBinder;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +22,8 @@ import com.einmalfel.earl.RSSCategory;
 import com.einmalfel.earl.RSSEnclosure;
 import com.einmalfel.earl.RSSItem;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
@@ -29,6 +35,34 @@ public class MainFeed extends AppCompatActivity {
     ListView mList;
     MyAdapter mAdapter;
     String link = ("http://rss.nytimes.com/services/xml/rss/nyt/World.xml");
+
+    /** List of RSS source URLs */
+    ArrayList<String> mySourceList;
+    /** Reference to main service, to access public methods */
+    MainService mService;
+    boolean mBound = false;
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        // Stuff to do after establishing connection
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            MainService.LocalBinder binder = (MainService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+
+            // Load the sources from file
+            mySourceList = mService.loadRSS();
+            for (String str : mySourceList)
+                Log.wtf("ASD", str);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +78,49 @@ public class MainFeed extends AppCompatActivity {
         // Create an adapter and attach it to the list
         mAdapter = new MyAdapter(this, myStrArr);
         mList.setAdapter(mAdapter);
+
+        // Start the MainService
+        Intent intent = new Intent(this, MainFeed.class);
+        startService(intent);
+    }
+
+    /** Bind to service if we are unbound. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        if (!mBound) {
+            Intent intent = new Intent(this, MainService.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        }
+    }
+
+    /** When stopping, save all sources in case we die. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveSources();
+        Log.wtf("STOP", "stopping");
     }
 
     @Override
     public void onResume() {
         super.onResume();
     }
-//This method populates the Options menu within the main activity
+
+    /** On destroy, save sources and unbind. */
+    @Override
+    protected void onDestroy () {
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+        }
+        Log.wtf("DESTROY", "Destroyinf");
+        saveSources();
+        super.onDestroy();
+    }
+
+    //This method populates the Options menu within the main activity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -120,6 +190,29 @@ public class MainFeed extends AppCompatActivity {
                 e.printStackTrace();
                 return null;
             }
+        }
+    }
+
+    /**
+     *  Save all RSS sources from mySourceList into a file on the phone.
+     */
+    private void saveSources() {
+        // Save the RSS source list to a file
+        try {
+            Log.i("FILEDIR", MainFeed.this.getFilesDir().toString());
+            File file = new File(MainFeed.this.getFilesDir(), getString(R.string.filename));
+            FileWriter writer = new FileWriter(file);
+            for (String str : mySourceList) {
+                writer.write(str + '\n');
+            }
+//            writer.write("test source 1\n");
+//            writer.write("test source 2\n");
+//            writer.write("test source 3\n");
+
+            writer.close();
+        }
+        catch (Exception e) {
+            Log.i("FILESAVE", "Failed to write to file: " + e.getMessage());
         }
     }
 }
---
+++ RSSNews/app/src/main/java/cs496_projecy/rssnews/MainService.java
@@ -0,0 +1,130 @@
+package cs496_projecy.rssnews;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.einmalfel.earl.EarlParser;
+import com.einmalfel.earl.Feed;
+import com.einmalfel.earl.Item;
+import com.einmalfel.earl.RSSItem;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by David on 5/1/2016.
+ * This file should
+ */
+public class MainService extends Service {
+    /** Create custom binder for onBind() */
+    private final IBinder mBinder = new LocalBinder();
+
+    /** List of RSS sources loaded from storage file */
+    ArrayList<String> sourceList = new ArrayList<String>();
+
+    /** The service is starting, due to a call to startService() */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    /** Returns a reference to this service so the Activity can access its public methods */
+    public class LocalBinder extends Binder {
+        MainService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return MainService.this;
+        }
+    }
+
+    /** A client is binding to the service with bindService() */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(this, "Serivce bound", Toast.LENGTH_LONG).show();
+        return mBinder;
+    }
+
+    /** Called when The service is no longer used and is being destroyed */
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        super.onDestroy();
+    }
+
+    /**
+     * Load RSS source URLs from a file stored on the phone.
+     * Puts all of the RSS sources into an array, which is returned.
+     * Does not download or check the URLs.
+     */
+    public ArrayList<String> loadRSS() {
+        // Open and read the file, line by line
+        try {
+            File file = new File(MainService.this.getFilesDir(), getString(R.string.filename));
+            InputStream in = new FileInputStream(file);
+            if (in != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+
+                // read the file line-by-line, adding each source to the sourceList
+                do {
+                    line = reader.readLine();
+
+                    if (!sourceList.contains(line) && (line != null)) {
+                        sourceList.add(line);
+                    }
+
+                    Log.i("FILEREAD", "Read line: " + line);
+                }
+                while (line != null);
+            }
+        } catch (Exception e) {
+            Log.i("FILELOAD", "Failed to laod file: " + e.getMessage());
+            Log.i("FILELOAD", "Stack Trace: " + e.getStackTrace());
+        }
+
+
+        return sourceList;
+    }
+
+    /**
+     * Get list of RSS items from the URLs stored in sourceList.
+     * Iterate through sourceList and
+     */
+    public ArrayList<RSSItem> getRSSItems(int start, int end) {
+
+        return null;
+    }
+
+    /**
+     * Download RSS data, and parse it into an RSSItem ArrayList.
+     */
+    private boolean downloadRSS(String url) {
+        try {
+            InputStream inputStream = new URL(url).openConnection().getInputStream();
+            Feed feed = EarlParser.parseOrThrow(inputStream, 0);
+            Log.i("ITEM", "Processing feed: " + feed.getTitle());
+            for (Item item : feed.getItems()) {
+                String title = item.getTitle();
+                Log.i("ITEM", "Item title: " + (title == null ? "N/A" : title));
+            }
+        }
+        catch (Exception e) {
+            Log.i("ITEM", "Failed to get feed" + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+}
