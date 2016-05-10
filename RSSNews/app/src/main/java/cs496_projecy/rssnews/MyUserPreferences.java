a991cc5f7ac438e17363fde7fe3328726833ffc2
kaiserh <lordbobsaget@gmail.com>
2016-05-10 12:16:10 -0700

    Pushing Current Changes on Settings before I shower and come back to it.

--- RSSNews/app/src/main/java/cs496_projecy/rssnews/MyUserPreferences.java
+++ RSSNews/app/src/main/java/cs496_projecy/rssnews/MyUserPreferences.java
@@ -1,14 +1,27 @@
 package cs496_projecy.rssnews;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.einmalfel.earl.RSSItem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,6 +46,7 @@ public class MyUserPreferences extends PreferenceActivity implements View.OnClic
 
     }
 
+
     @Override
     public void onClick (View v){
             startActivity(new Intent(this, MainFeed.class));
@@ -41,7 +55,9 @@ public class MyUserPreferences extends PreferenceActivity implements View.OnClic
     @Override
     protected boolean isValidFragment(String fragmentName)
     {
-        return PreferenceFragment.class.getName().equals(fragmentName);
+        return PreferenceFragment.class.getName().equals(fragmentName) ||
+                Prefs1Fragment.class.getName().equals(fragmentName) ||
+                Prefs2Fragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -60,11 +76,14 @@ public class MyUserPreferences extends PreferenceActivity implements View.OnClic
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
 
-            // Make sure default values are applied.  In a real app, you would
-            // want this in a shared function that is used to retrieve the
-            // SharedPreferences wherever they are needed.
-            PreferenceManager.setDefaultValues(getActivity(),
-                    R.xml.advanced_preferences, false);
+            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+            if (sharedPref == null) {
+                // Make sure default values are applied.  In a real app, you would
+                // want this in a shared function that is used to retrieve the
+                // SharedPreferences wherever they are needed.
+                PreferenceManager.setDefaultValues(getActivity(),
+                        R.xml.advanced_preferences, false);
+            }
 
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.fragmented_preferences);
@@ -75,32 +94,81 @@ public class MyUserPreferences extends PreferenceActivity implements View.OnClic
      * This fragment contains a second-level set of preference that you
      * can get to by tapping an item in the first preferences fragment.
      */
-    public static class Prefs1FragmentInner extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            // Can retrieve arguments from preference XML.
-            Log.i("args", "Arguments: " + getArguments());
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.fragmented_preferences_inner);
-        }
-    }
-
-    /**
-     * This fragment shows the preferences for the second header.
-     */
     public static class Prefs2Fragment extends PreferenceFragment {
+
+        private List<RSSItem> rssItemList = new ArrayList<>();
+
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+        // Can retrieve arguments from headers XML.
+            Log.i("args","Arguments: "+ getArguments());
+
+        // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.fragmented_preferences_inner);
+
+        }
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            return inflater.inflate(R.layout.feed_list, null);
+        }
+
+        private void addRSSLink() {
+            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+        }
+/*
         @Override
         public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
 
-            // Can retrieve arguments from headers XML.
-            Log.i("args", "Arguments: " + getArguments());
 
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.preference_dependencies);
+            Button btnOk = (Button) (this.findViewById(R.id.btnOK));
+            btnOk.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    validateSaveExit();
+                }
+            });
+        }
+
+        private String checkNonEmpty(int id, String info) {
+            TextView textView = (TextView) (findViewById(id));
+            if (textView != null) {
+                CharSequence txt = textView.getText();
+                if (txt != null) {
+                    int len = txt.length();
+                    if (len > 0) {
+                        return txt.toString();
+                    }
+                }
+                textView.setHint(info + " is required");
+            }
+            return null;
+        }
+
+        private void validateSaveExit() {
+            String author = checkNonEmpty(R.id.txtRSSLink, "author");
+            String title = checkNonEmpty(R.id.txtTitle, "title");
+            if (author != null && title != null) {
+                // return result
+
+            }
+*/
+    }
+/*
+    private class MyTask extends AsyncTask<String, Float, ArrayList<RSSItem>> {
+
+        @Override
+        protected ArrayList<RSSItem> doInBackground(String... params) {
+            try {
+                File file = new File(this.getFilesDir(), getString(R.string.filename));
+                InputStream in = new FileInputStream(file);
+            } catch (Exception e) {
+
+            }
+            return null;
         }
     }
+*/
+
 }
